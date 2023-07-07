6a9feb4012ea2160ca5fb25d994a92bf01fff7f5
Sourabh Hodage <106645340+sourabh-hodage@users.noreply.github.com>
2023-07-07 06:47:11 +0000

    new design

--- src/components/AHeader.tsx
+++ src/components/AHeader.tsx
@@ -1,27 +1,80 @@
-import { JOB_TITLE, LOCATION, NAME } from "../config/Config"
+import { JOB_TITLE, CONTACT, LOCATION, NAME, PHNUM } from "../config/Config";
 
 export const Header = () => {
-    return (
-        <header className="inline-flex items-baseline justify-between w-full mb-3 align-top border-b-8">
-            <div className="block">
-                <h1 className="mb-0 text-5xl font-bold text-gray-750">
-                    {NAME}
-                </h1>
-                {/*Job Title-------------------------------------------------------------------------------------------------------*/}
-                <h2 className="m-0 ml-2 text-2xl font-semibold text-gray-700 leading-snugish">
-                    {JOB_TITLE}
-                </h2>
-                {/*Location -------------------------------------------------------------------------------------------------------*/}
-                <h3 className="m-0 mt-2 ml-2 font-semibold text-md text-gray-550 leading-snugish">
-                    {LOCATION}
-                </h3>
-            </div>
-            {/*   Initials Block         
-            <div className="justify-between px-3 mt-0 mb-5 text-3xl font-bold leading-none text-gray-200 initials-container bg-gray-750 print:bg-black" style={{ paddingBottom: '1.5rem', paddingTop: '1.5rem' }}>
-                <div className="text-center initial">T</div>
-                <div className="text-center initial">L</div>
-                <div className="text-center initial">H</div>
-            </div>*/}
-        </header>
-    )
-}
+  return (
+    <header className="inline-flex items-baseline justify-between w-full mb-3 align-top border-b-8">
+      <div className="block">
+        <h1 className="mb-0 text-5xl font-bold text-gray-750">{NAME}</h1>
+
+        {/*Job Title-------------------------------------------------------------------------------------------------------*/}
+
+        <h2 className="m-0 ml-2 text-2xl font-semibold text-gray-700 leading-snugish">
+          {JOB_TITLE}
+        </h2>
+
+        {/*Location -------------------------------------------------------------------------------------------------------*/}
+
+        <h3 className="m-0 mt-2 ml-2 font-semibold text-md text-gray-550 leading-snugish">
+          {LOCATION}
+        </h3>
+
+        <h3 className="m-0 mt-2 ml-2 font-semibold text-md text-gray-550 leading-snugish">
+          {PHNUM}
+        </h3>
+      </div>
+
+      {/* Initials Block  
+
+<div className="justify-between px-3 mt-0 mb-5 text-3xl font-bold leading-none text-gray-200 initials-container bg-gray-750 print:bg-black" style={{ paddingBottom: '1.5rem', paddingTop: '1.5rem' }}> 
+
+<div className="text-center initial">T</div> 
+
+<div className="text-center initial">L</div> 
+
+<div className="text-center initial">H</div> 
+
+</div>*/}
+
+      <section className="sect first:mt-0">
+        {/* To keep in the same column ------------------------------------------------------------------------*/}
+
+        <div className="break-inside-avoid">
+          <section className="pb-2 mb-2 border-b-2 break-inside-avoid">
+            <ul className="list-inside pr-7">
+              {CONTACT.map((contact, index) => {
+                return (
+                  <li
+                    key={index}
+                    className="mt-1 leading-normal transition duration-100 ease-in text-gray-550 text-md hover:text-gray-700"
+                  >
+                    <a
+                      href={contact.link}
+                      rel="noreferrer"
+                      target={"_blank"}
+                      className="group"
+                    >
+                      <span
+                        className="text-lg font-semibold text-gray-700 leading-snugish"
+                        style={{
+                          marginRight: contact.margin ? contact.margin : 0,
+                        }}
+                      >
+                        {contact.name}:
+                      </span>
+
+                      {contact.value}
+
+                      <span className="inline-block font-normal transition duration-100 ease-in text-gray-550 print:text-black group-hover:text-gray-700">
+                        ↗
+                      </span>
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </section>
+        </div>
+      </section>
+    </header>
+  );
+};
--- src/config/Config.tsx
+++ src/config/Config.tsx
@@ -2,207 +2,201 @@
 export const NAME = "Sourabh Hodage";
 export const JOB_TITLE = "Aspiring Data Analyst";
 export const LOCATION = "Gadhinglaj, Maharashtra";
+// export const EMAIL = "sshodage3292@gmail.com";
+export const PHNUM = "+91 92600 93292";
 
 // B
 export const CONTACT = [
-    {
-        "name": "Github",
-        "link": "https://github.com/sourabh-hodage",
-        "value": "github/sourabh-hodage",
-        "icon": "",
-        "margin": "32px"
-    },
-    {
-        "name": "Linkedin",
-        "link": "https://www.linkedin.com/in/sourabh-hodage/",
-        "value": "linkedin/sourabh-hodage",
-        "icon": "",
-        "margin": "22px"
-    },
-    {
-        "name": "Email",
-        "link": "mailto:sshodage3292@gmail.com",
-        "value": "sshodage3292@gmail.com",
-        "icon": "",
-        "margin": "40px"
-    },
-    {
-        "name": "Phone",
-        "link": "tel:+919260093292",
-        "value": "+919260093292",
-        "icon": "",
-        "margin": "34px"
-    },
-    {
-        "name": "HackerRank",
-        "link": "https://www.hackerrank.com/sourabhhodage",
-        "value": "hackerrank/sourabhhodage",
-        "icon": "",
-        "margin": "5px"
-    }
-  
-]
+  {
+    name: "Github",
+    link: "https://github.com/sourabh-hodage",
+    value: "github/sourabh-hodage",
+    icon: "",
+    margin: "32px",
+  },
+  {
+    name: "Linkedin",
+    link: "https://www.linkedin.com/in/sourabh-hodage/",
+    value: "linkedin/sourabh-hodage",
+    icon: "",
+    margin: "22px",
+  },
+  {
+    name: "Email",
+    link: "sshodage3292@gmail.com",
+    value: "sshodage3292@gmail.com",
+    icon: "",
+    margin: "40px",
+  },
+  // {
+  // "name": "Phone",
+  // "link": "tel:+919260093292",
+  // "value": "+919260093292",
+  // "icon": "",
+  // "margin": "34px"
+  // },
+  {
+    name: "HackerRank",
+    link: "https://www.hackerrank.com/sourabhhodage",
+    value: "hackerrank/sourabhhodage",
+    icon: "",
+    margin: "5px",
+  },
+];
 
 // C
-export const SUMMARY = "Recent graduate with a strong foundation in data analysis and machine learning from Imarticus Learning. Proficient in SQL, Python (Pandas, NumPy, Seaborn, Matplotlib) and Power BI with a passion for solving real-world problems through data-driven approaches. Seeking a data analyst role where I can apply my skills and continue learning in a dynamic and collaborative environment."
+export const SUMMARY =
+  "Post graduate with a strong foundation in data analysis and machine learning from Imarticus Learning. Proficient in SQL, Python, Excel and Power BI with a passion for solving real-world problems through data-driven approaches. Seeking a data analyst role where I can apply my skills and continue learning in a dynamic and collaborative environment.";
+
+  
 // D
 export const EXPERIENCE = [
-    {
-        "title": "Internship",
-        "date": "Jan 2022 - June 2022",
-        "company": "Technorrithm Engineering Pvt.Ltd",
-        "bullets": ["Designing of Bulk Milk Cooler",
-            "Analysis Field in Products",
-            "Business related visualizations",
-            "Finding Thermodynamic Conditions applied on it.",
-            "Preparing a standard Design which can affordable for customers as well as Small Scale Industries"
-        ]
-    },
-
-]
+  {
+    title: "Internship",
+    date: "Jan 2022 - June 2022",
+    company: "Technorrithm Engineering Pvt.Ltd",
+    bullets: [
+      "Designing of Bulk Milk Cooler",
+      "Analysis Field in Products",
+      "Business related visualizations",
+      "Finding Thermodynamic Conditions applied on it.",
+      "Preparing a standard Design which can affordable for customers as well as Small Scale Industries",
+    ],
+  },
+];
 
 // E
 export const EDUCATION = [
-    {
-        "title": "Post Graduate Program in Data Analytics",
-        "date": "February 2022",
-        "company": "Imarticus Learning,Pune",
-        "details": [
-            {
-                "title": "Specilization",
-                "description": "MS SQL,Python,Machine Learning,Deep Learning,Power BI, Tableau "
-            }
-        ]
-    },
-    {
-        "title": "BTech in Mechanical Engineering",
-        "date": "June 2022",
-        "company": "Rajarambapu Institute of Technology, Islampur",
-        "details": [
-            {
-                "title": "Grade",
-                "description": "First Class, 6.75 CGPI"
-            },
-        ]
-    },
-
-
-]
-
-
+  {
+    title: "Post Graduate Program in Data Analytics",
+    date: "May 2023",
+    company: "Imarticus Learning,Pune",
+    details: [
+      {
+        title: "Specilization",
+        description:
+          "MS SQL,Python,Machine Learning,Deep Learning,Power BI, Tableau ",
+      },
+    ],
+  },
+  {
+    title: "BTech in Mechanical Engineering",
+    date: "June 2022",
+    company: "Rajarambapu Institute of Technology, Islampur",
+    details: [
+      {
+        title: "Grade",
+        description: "First Class, 6.75 CGPI",
+      },
+    ],
+  },
+];
 
 // F
 export const PROJECTS = [
-    {
-        "title": "Loan Prediction Model ",
-        "url": "https://github.com/sourabh-hodage/Logistic-Regression-Model",
-        "github": "https://github.com/sourabh-hodage/Logistic-Regression-Model",
-        "date": "June 2022",
-        "technologies": [
-            "Python",
-            "Machine Learning",
-            "visualization",
-            "Pandas",
-            "Numpy",
-            "Seaborn",
-            "Sklearn"
-
-        ],
-        "specs": [
-            "Loan Prediction Model by using Logistic Regression Model",
-            "Study of Bank data of customers",
-            "In this Data I had worked whether customers will approved load or not", 
-            "Find out the factors effecting for approval of loan",
-            "Study of Background of Customers for approval of loan", 
-            "And Prepared a model with accuracy of 89%"
-        ]
-    },
-    {
-        "title": "Customer Churn Prediction ",
-        "url": "https://github.com/sourabh-hodage/Random-Forest-model-of-Churn-Customers",
-        "github": "https://github.com/sourabh-hodage/Random-Forest-model-of-Churn-Customers",
-        "date": "August 2022",
-        "technologies": [
-            "Python",
-            "Machine Learning",
-            "visualization",
-            "Pandas",
-            "Numpy",
-            "Seaborn",
-            "Sklearn"
-
-        ],
-        "specs": [
-            "By using Random Forest (Classification) Model",
-            "Study of Churn Data for prediction of customer",
-            "In this project I have worked on Churn Data for predicting that customers will churn or not",
-            "Churn status is classified as Yes or No",
-            "I worked on this data to find out what are factors affecting the status for the customers to churn the company"
-        ]
-    },
-    {
-        "title": "Music Recommendation System on Spotify",
-        "url": "https://github.com/sourabh-hodage/by-using-K-Means-Music-Recommendation-System-on-Spotif-",
-        "github": "https://github.com/sourabh-hodage/by-using-K-Means-Music-Recommendation-System-on-Spotif-",
-        "date": "November 2022",
-        "technologies": [
-            "Python",
-            "Machine Learning",
-            "visualization",
-            "Pandas",
-            "Numpy",
-            "Seaborn",
-            "Sklearn",
-            "K-Means"
-
-        ],
-        "specs": [
-            "By using K-Means Clustering Model and Euclidian distance method",
-            "Spotify Recommendation Engine for creating playlists based on user behavior",
-            "Non-Negative Matrix Factorization (NFM) For Decomposing Data",
-            "For for predicating the top 5 users and the top 5 songs heard and evaluating it using K-Means clustering"
-        ]
-
-    }
-]
+  {
+    title: "Zomato Business Analysis ",
+    url: "",
+    github: "",
+    date: "October 2022",
+    technologies: [
+      "SQL",
+    ],
+    specs: [
+      "Applied advanced SQL queries and joins to analyze key metrics and trends in customer ratings, revenue growth, and user engagement.",
+      "Contributed to actionable recommendations resulting in enhanced customer satisfaction and optimized marketing strategies.",
+      "Finding total profit through Premium Customers.",
+      "Identifing Best Food as per restaurent and apply some discount and coupon codes on less selling items",
+    ],
+  },
+  {
+    title: "Customer Churn Prediction ",
+    url: "https://github.com/sourabh-hodage/Random-Forest-model-of-Churn-Customers",
+    github:
+      "https://github.com/sourabh-hodage/Random-Forest-model-of-Churn-Customers",
+    date: "November 2022",
+    technologies: [
+      "Python",
+      "Machine Learning",
+      "visualization",
+      "Pandas",
+      "Numpy",
+      "Seaborn",
+      "Sklearn",
+    ],
+    specs: [
+      "By using Random Forest (Classification) Model",
+      "Study of Churn Data for prediction of customer",
+      "In this project I have worked on Churn Data for predicting that customers will churn or not",
+      "Churn status is classified as Yes or No",
+      "I worked on this data to find out what are factors affecting the status for the customers to churn the company",
+    ],
+  },
+  {
+    title: "Music Recommendation System on Spotify",
+    url: "https://github.com/sourabh-hodage/by-using-K-Means-Music-Recommendation-System-on-Spotif-",
+    github:
+      "https://github.com/sourabh-hodage/by-using-K-Means-Music-Recommendation-System-on-Spotif-",
+    date: "January 2023",
+    technologies: [
+      "Python",
+      "Machine Learning",
+      "visualization",
+      "Pandas",
+      "Numpy",
+      "Seaborn",
+      "Sklearn",
+      "K-Means",
+    ],
+    specs: [
+      "By using K-Means Clustering Model and Euclidian distance method",
+      "Spotify Recommendation Engine for creating playlists based on user behavior",
+      "Non-Negative Matrix Factorization (NFM) For Decomposing Data",
+      "For for predicating the top 5 users and the top 5 songs heard and evaluating it using K-Means clustering",
+    ],
+  },
+];
 
 // G
 export const SKILLS = [
-    {
-        "title": "Soft Skills ",
-        "skills": [
-            "Problem Solving","Team Work","Decision Making ","Leadership ","Quick Learning Ability"
-
-        ]
-    },
-    {
-        "title": "Languages",
-        "skills": [
-            "Python", "SQL", "C Language"
-        ]
-    },
-    {
-        "title": "Data Analysis ",
-        "skills": [
-            "SQL (MS SQL Server) ",
-            "Python",
-            "ML & DL",
-            "MS Office Tools",
-            "Tableau",
-            "Power BI"
-        ]
-    },
-    {
-        "title": "Tools & Packages",
-        "skills": [
-            "Linear Regression", "Logistic Regression",
-            "Decision Tree",
-            "Random Forest",
-            "PCA", "K-Means Clustering",
-            "Pandas", "Numpy",
-            "Seaborn"
-
-        ]
-    },
- 
-]
+  {
+    title: "Soft Skills ",
+    skills: [
+      "Problem Solving",
+      "Team Work",
+      "Decision Making ",
+      "Leadership ",
+      "Quick Learning Ability",
+    ],
+  },
+  {
+    title: "Languages",
+    skills: ["Python", "SQL", "C Language"],
+  },
+  {
+    title: "Data Analysis ",
+    skills: [
+      "SQL (MS SQL Server) ",
+      "Python",
+      "Machine Learning",
+      "Deep Learning",
+      "MS Office Tools",
+      "Tableau",
+      "Power BI",
+    ],
+  },
+  {
+    title: "Tools & Packages",
+    skills: [
+      "Linear Regression",
+      "Logistic Regression",
+      "Decision Tree",
+      "Random Forest",
+      "PCA",
+      "K-Means Clustering",
+      "Pandas",
+      "Numpy",
+      "Seaborn",
+    ],
+  },
+];
